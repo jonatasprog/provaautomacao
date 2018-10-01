01993dfe51ee019a64d81f3b4cb481ce763720cf
Jônatas Deorce <jonatasprog@gmail.com>
2018-10-01 16:37:33 -0300

    Evoluindo a estrutura do projeto. Feature e Steps pré-configurado.

---
+++ src/test/java/com/provaautomacao/runner/TestRunner.java
@@ -0,0 +1,16 @@
+package com.provaautomacao.runner;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+
+    @RunWith(Cucumber.class)
+    @CucumberOptions(
+            features = "/src/test/java/com.provaautomacao/features/ComprarProduto.feature",
+            glue = {"steps"}
+            //format = {"pettry", "html:test-outout"}
+    )
+    public class TestRunner {
+    }
+
---
+++ src/test/java/com/provaautomacao/steps/ComprarProduto.java
@@ -0,0 +1,59 @@
+package com.provaautomacao.steps;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class ComprarProduto {
+
+    @Given("^Usuário está na Home Page$")
+    public void usuário_está_na_Home_Page(){
+
+    }
+
+    @When("^Usuário escolhe um produto$")
+    public void usuário_escolhe_um_produto(){
+
+    }
+
+    @Then("^É redirecionado para a página de descrição do produto$")
+    public void é_redirecionado_para_a_página_de_descrição_do_produto(){
+
+    }
+
+    @Given("^Usuário está na página de descrição de seu produto$")
+    public void usuário_está_na_página_de_descrição_de_seu_produto() {
+
+    }
+
+    @When("^Usuário adiciona seu produto no carrinho$")
+    public void usuário_adiciona_seu_produto_no_carrinho() {
+
+    }
+
+    @Then("^O usuário vai para a página de confirmação de seu item no carrinho$")
+    public void o_usuário_vai_para_a_página_de_confirmação_de_seu_item_no_carrinho() {
+
+    }
+
+    @Given("^Usuário está na pagina de confirmação$")
+    public void usuário_está_na_pagina_de_confirmação() {
+
+    }
+
+    @Given("^Procede para o checkout$")
+    public void procede_para_o_checkout() {
+
+    }
+
+    @When("^O usuário confirma o valor da compra$")
+    public void o_usuário_confirma_o_valor_da_compra() {
+
+    }
+
+    @Then("^O usuário finaliza a compra$")
+    public void o_usuário_finaliza_a_compra() {
+
+    }
+
+}
